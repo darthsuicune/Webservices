ddaf29c17832929b210d097e0c93427565ccf7c9
Denís <lapuente@abbyy.com>
2013-09-25 11:00:40 +0200

    Remove useless information from main printing

--- index.php
+++ index.php
@@ -7,6 +7,4 @@
     include_once 'Webserver.php';
     $server = new Webserver();
     echo "{response:".$server->parseRequest()."}";
-    foreach ($_SERVER as $key => $value) {
-    	echo "\n<br>$key => $value";
-    }
+
