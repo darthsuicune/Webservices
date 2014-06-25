fef3607a2bdb106efe9c749648ce190112560374
Denís <lapuente@abbyy.com>
2014-06-25 11:16:48 +0200

    Add defualt config

---
+++ config.default.php
@@ -0,0 +1,7 @@
+<?php
+const CHARSET = "utf8";
+const DB_DATABASE = "database_name";
+const DB_ADDRESS = "database_host";
+const DB_USERNAME = "database_username";
+const DB_PASSWORD = "database_password";
+const LOG_FILE = "/path/to/log";
